389cbd9211c86cc96322d8a1e6aa8f4a97c5d279
Mei <62211361+jmaetogonon@users.noreply.github.com>
2020-09-16 10:17:32 +0800

    Add files via upload

---
+++ Bonus Solution_Mabasa/Bonus Solution_Mabasa/Bonus Solution/Bonus Project/Main Form.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSales = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblBonus = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtCode = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(95, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "&Sales:"
+        '
+        'txtSales
+        '
+        Me.txtSales.Location = New System.Drawing.Point(99, 53)
+        Me.txtSales.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtSales.Name = "txtSales"
+        Me.txtSales.Size = New System.Drawing.Size(104, 27)
+        Me.txtSales.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(233, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Bonus:"
+        '
+        'lblBonus
+        '
+        Me.lblBonus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBonus.Location = New System.Drawing.Point(236, 53)
+        Me.lblBonus.Name = "lblBonus"
+        Me.lblBonus.Size = New System.Drawing.Size(96, 29)
+        Me.lblBonus.TabIndex = 7
+        Me.lblBonus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(90, 106)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(86, 32)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(182, 106)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(83, 32)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtCode
+        '
+        Me.txtCode.Location = New System.Drawing.Point(26, 54)
+        Me.txtCode.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtCode.Name = "txtCode"
+        Me.txtCode.Size = New System.Drawing.Size(44, 27)
+        Me.txtCode.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "C&ode:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(355, 166)
+        Me.Controls.Add(Me.txtCode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblBonus)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSales)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Bonus Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtSales As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblBonus As System.Windows.Forms.Label
+    Friend WithEvents btnCalc As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents txtCode As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Bonus Solution_Mabasa/Bonus Solution_Mabasa/Bonus Solution/Bonus Project/Main Form.vb
@@ -0,0 +1,67 @@
+﻿' Name:         Bonus Project
+' Purpose:      Display a bonus amount
+' Programmer:   Jochelle Mae T. Mabasa on September 03, 2020
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtSales_Enter(sender As Object, e As EventArgs) Handles txtSales.Enter
+        txtSales.SelectAll()
+    End Sub
+
+    Private Sub txtCode_Enter(sender As Object, e As EventArgs) Handles txtCode.Enter
+        txtCode.SelectAll()
+    End Sub
+    Private Sub txtSales_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtCode.KeyPress, txtSales.KeyPress
+        ' allows the text box to accept only numbers and  the Backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub ClearBonus(sender As Object, e As EventArgs) Handles txtCode.TextChanged, txtSales.TextChanged
+        lblBonus.Text = String.Empty
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' calculates a bonus amount
+
+        Dim intCode As Integer
+        Dim dblSales As Double
+        Dim dblBonus As Double
+        Dim dblRate As Double
+
+        Integer.TryParse(txtCode.Text, intCode)
+        Double.TryParse(txtSales.Text, dblSales)
+
+        Select Case intCode
+            Case 1 To 5
+                dblRate = 0.03
+                dblBonus = dblSales * dblRate
+                lblBonus.Text = dblBonus.ToString
+            Case 6
+                dblRate = 0.07
+                dblBonus = dblSales * dblRate
+                lblBonus.Text = dblBonus.ToString
+            Case 7 To 10
+                dblRate = 0.12
+                dblBonus = dblSales * dblRate
+                lblBonus.Text = dblBonus.ToString
+            Case Else
+                lblBonus.Text = "Invalid code"
+        End Select
+
+        txtCode.Focus()
+
+
+    End Sub
+End Class
